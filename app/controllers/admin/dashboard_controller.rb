07b2ab090d13a260c2ebfab9f8002abbedae525e
IamHPark <heesoo.p1023@gmail.com>
2022-06-02 15:33:12 +0000

    add dashboard count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @products_size = Product.all.count
+    @categories_size = Category.all.count
   end
 end
