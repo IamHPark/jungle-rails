7e1fa1244f5e0df4bd3fa793dedd5ff2e7d59c42
IamHPark <heesoo.p1023@gmail.com>
2022-06-03 01:02:13 +0000

    add users routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,14 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resources :users, only: [:new, :create]
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
