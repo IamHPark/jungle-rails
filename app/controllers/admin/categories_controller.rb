c504290e204ac5f22c1edb5c252489d2181d88b9
IamHPark <heesoo.p1023@gmail.com>
2022-06-03 00:34:35 +0000

    add create category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -8,6 +8,13 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(params.require(:category).permit(:name))
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category created"
+    else
+      render :index
+    end
   end
 
 end
