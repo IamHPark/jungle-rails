ddb39dbde371fcfbe2b4a3db381fa51856b76603
IamHPark <heesoo.p1023@gmail.com>
2022-06-08 03:12:34 +0000

    user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # gives our User model authentication methods via bcrypt
   has_secure_password
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, :uniqueness => {:case_sensitive => false}
+  validates :password, length: {minimum: 5}
+
+  def authenticate_with_credentials(params[:email], params[:password])
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      user
+    end
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  before(:each) do
+    @attr = { :first_name => "John",
+              :last_name => "Smith",
+              :email => "test@test.com",
+              :password => "secret"
+              #:password_confirmation => "secret"
+            }
+  end
+
+  describe 'Validation' do
+    it "should require a matching password confirmation" do
+      user = User.new(@attr.merge(:password_confirmation => "invalid"))
+      expect(user).not_to be_valid
+    end
+    it "should be case-insensitive with email" do
+      user1 = User.create(@attr)
+      user2 = User.new(@attr.merge(:email => "TEST@test.com"))
+      expect(user2).not_to be_valid
+    end
+    it 'should have first_name' do
+      user = User.create
+      expect(user.errors.full_messages).to include("First name can't be blank")
+    end
+    it 'should have last_name' do
+      user = User.create
+      expect(user.errors.full_messages).to include("Last name can't be blank")
+    end
+    it 'should have eamil' do
+      user = User.create
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "should more than 5 passwords" do
+      user = User.new(@attr.merge(:password => "1234"))
+      expect(user).not_to be_valid
+    end
+  end
+end
